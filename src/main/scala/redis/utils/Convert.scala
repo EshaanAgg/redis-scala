414eccfa543d62a9af4246b35b11fba8c8af4dc4
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2025-06-19 18:59:25 +0530

    add 8 bytes offset in parsing

--- src/main/scala/redis/utils/Convert.scala
+++ src/main/scala/redis/utils/Convert.scala
@@ -4,21 +4,23 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 object Convert:
-  def getLENumber(buf: Array[Byte]): Int =
+  def getLENumber(buf: Array[Byte]): Long =
     buf.length match
-      case 1 => buf(0).toInt
+      case 1 => buf(0).toLong
       case 2 => ((buf(0) & 0xff) << 8) | (buf(1) & 0xff)
       case 4 => ByteBuffer.wrap(buf).order(ByteOrder.LITTLE_ENDIAN).getInt
+      case 8 => ByteBuffer.wrap(buf).order(ByteOrder.LITTLE_ENDIAN).getLong
       case _ =>
         throw new IllegalArgumentException(
           s"Unsupported length for getLENumber: ${buf.length}"
         )
 
-  def getBENumber(buf: Array[Byte]): Int =
+  def getBENumber(buf: Array[Byte]): Long =
     buf.length match
       case 1 => buf(0).toInt
       case 2 => ((buf(1) & 0xff) << 8) | (buf(0) & 0xff)
       case 4 => ByteBuffer.wrap(buf).order(ByteOrder.BIG_ENDIAN).getInt
+      case 8 => ByteBuffer.wrap(buf).order(ByteOrder.BIG_ENDIAN).getLong
       case _ =>
         throw new IllegalArgumentException(
           s"Unsupported length for getBENumber: ${buf.length}"
